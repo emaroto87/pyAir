c9d0903fd33e2be9bff6f4e5522c13a84a9d9755
emaroto87 <e.maroto.py@gmail.com>
2026-06-04 12:55:31 +0200

    Add files via upload

---
+++ Github/01_References_and_Methods/Methods/Airbus_Software/ANCOlite/ANCOlite.bat
@@ -0,0 +1,7 @@
+echo off
+rem ****************************************************************************
+rem * Para ejecutarlo, desde MS-DOS, con la carpeta activa en la ruta de red,  *
+rem * y asi poder cargar el archivo .ini de inicializacion en el arranque, se  *
+rem * ha creado alli un acceso directo al ejecutable al que apunta este script *
+rem ****************************************************************************
+start \\efs\InfoCalculo\Soft\ANCOlite\ANCOlite.lnk

58389c3faaaea8f041742bab7f5fa2d0bbe25dad
emaroto87 <e.maroto.py@gmail.com>
2026-06-04 13:08:10 +0200

    Add files via upload

---
+++ Github/01_References_and_Methods/Methods/Airbus_Software/Fitan/cozzone.bat
@@ -0,0 +1,8 @@
+echo off
+
+rem Script/Batch lanzador de programa a interprete python
+rem ¡¡IMPORTANTE!! El interprete de Python 2.X debe estar ya instalado
+rem ¡¡IMPORTANTE!! Actualizar PATH al directorio de red de programas python (segun plataforma)
+
+path "\\multivac2\calculo-documentacion\python\apps\cozzone\"
+cozzone.py

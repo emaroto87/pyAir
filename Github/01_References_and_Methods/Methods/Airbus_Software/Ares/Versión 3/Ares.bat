edbf226bdd4938a74e80a09d824d7d58c54fdd8e
emaroto87 <e.maroto.py@gmail.com>
2026-06-04 13:00:03 +0200

    Add files via upload

---
+++ Github/01_References_and_Methods/Methods/Airbus_Software/Ares/Versión 3/Ares.bat
@@ -0,0 +1,12 @@
+echo off
+cls
+echo.
+echo *******************************************************************************
+echo ******************        PROGRAMA ARES 3.0: ATENCION        ******************
+echo *******************************************************************************
+echo.
+echo 1 MODULO DE ANALISIS BIDIMENSIONAL DE ARES DESAUTORIZADO HASTA NUEVO AVISO
+echo 2 NO REALIZAR PASADAS SUCESIVAS DE DIFERENTES ENTRADAS CON LA INTERFAZ GRAFICA
+echo.
+pause
+START \\CAE\InfoCalculo\Soft\Ares\Ares.exe
